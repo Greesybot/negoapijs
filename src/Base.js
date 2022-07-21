5162e2f4ab67a2692e35667440725d8900649ace
Nego <61694826+kardespro@users.noreply.github.com>
2022-07-21 17:21:03 +0400

    Update Base.js

--- src/Base.js
+++ src/Base.js
@@ -4,13 +4,16 @@
 *
 */
 
-
+const os = require("node:os");
 class Base_UTIL{
 
 gateway(){
 return "https://gateway.nego-dev.com && https://gateway.nego.gg"
 }
 
+networkInterface(){
+return os.networkInterfaces()
+}
 
 }
 
